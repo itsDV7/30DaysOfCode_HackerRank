a0258169a0d6d67085d50cc70d46a599169ec967
Divyansh Chaudhary <divyansh7c@gmail.com>
2022-03-04 06:06:35 +0530

    Create Day0.cpp

---
+++ Day0.cpp
@@ -0,0 +1,14 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+// #include <string>
+
+int main() {
+    std::cout << "Hello, World.\n";
+    std::string s;
+    getline(std::cin, s);
+    std::cout << s;
+    return 0;
+}
