2ffea38b2ec58c10aa37087edda60bb2d656b3c7
Divyansh Chaudhary <divyansh7c@gmail.com>
2022-03-17 01:58:13 +0530

    Create Day12.cpp

---
+++ Day12.cpp
@@ -0,0 +1,90 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+
+class Person{
+	protected:
+		string firstName;
+		string lastName;
+		int id;
+	public:
+		Person(string firstName, string lastName, int identification){
+			this->firstName = firstName;
+			this->lastName = lastName;
+			this->id = identification;
+		}
+		void printPerson(){
+			cout<< "Name: "<< lastName << ", "<< firstName <<"\nID: "<< id << "\n"; 
+		}
+	
+};
+
+class Student :  public Person{
+	private:
+		vector<int> testScores;  
+	public:
+        /*	
+        *   Class Constructor
+        *   
+        *   Parameters:
+        *   firstName - A string denoting the Person's first name.
+        *   lastName - A string denoting the Person's last name.
+        *   id - An integer denoting the Person's ID number.
+        *   scores - An array of integers denoting the Person's test scores.
+        */
+        // Write your constructor here
+        Student(string fName, string lName, int id, vector<int> scores):Person(fName, lName, id){
+            this->testScores = scores;
+        }
+        /*	
+        *   Function Name: calculate
+        *   Return: A character denoting the grade.
+        */
+        // Write your function here
+        string calculate(){
+            float grade{};
+            int sum_marks{0};
+            for (int i: testScores){
+                sum_marks += i;
+            }
+            grade = sum_marks / testScores.size();
+            if(grade >= 90){
+                return "O";
+            }
+            else if(grade >= 80){
+                return "E";
+            }
+            else if(grade >= 70){
+                return "A";
+            }
+            else if(grade >= 55){
+                return "P";
+            }
+            else if(grade >= 40){
+                return "D";
+            }
+            else{
+                return "T";
+            }
+        }
+};
+
+int main() {
+	string firstName;
+  	string lastName;
+	int id;
+  	int numScores;
+	cin >> firstName >> lastName >> id >> numScores;
+  	vector<int> scores;
+  	for(int i = 0; i < numScores; i++){
+	  	int tmpScore;
+	  	cin >> tmpScore;
+		scores.push_back(tmpScore);
+	}
+	Student* s = new Student(firstName, lastName, id, scores);
+	s->printPerson();
+	cout << "Grade: " << s->calculate() << "\n";
+	return 0;
+}
